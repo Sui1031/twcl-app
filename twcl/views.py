f1b5d35cf58b24ff33eb5003e199f431545fc351
sui1031 <sui1031ny@gmail.com>
2021-08-17 01:27:16 +0900

    'サインアップ

--- twcl/views.py
+++ twcl/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def signup(request):
+  if request.method == 'POST':
+    Form = UserCreationForm(request.POST)
+    if Form.is_valid():
+      Form.save()
+
+      return redirect('twcl:home')
+  else:
+    Form = UserCreationForm()
+
+  return render(request, 'twcl/signup.html', {'form': Form})
+
+def showHome(request):
+  return render(request, 'twcl/home.html')
